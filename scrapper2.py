5121aef02a94ab8784efdb2733004011da411fdf
woodyvalentino123 <69005803+woodyvalentino123@users.noreply.github.com>
2022-04-27 16:47:42 +0530

    Add files via upload

---
+++ scrapper2.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import pandas as pd
+
+
+bright_stars_url = 'https://en.wikipedia.org/wiki/List_of_brown_dwarfs'
+
+page = requests.get(bright_stars_url)
+print(page)
+
+soup = bs(page.text,'html.parser')
+
+star_table = soup.find('table')
+
+temp_list= []
+table_rows = star_table.find_all('tr')
+for tr in table_rows:
+    td = tr.find_all('td')
+    row = [i.text.rstrip() for i in td]
+    temp_list.append(row)
+
+
+
+Star_names = []
+Distance =[]
+Mass = []
+Radius =[]
+
+for i in range(1,len(temp_list)):
+    Star_names.append(temp_list[i][0])
+    Distance.append(temp_list[i][5])
+    Mass.append(temp_list[i][7])
+    Radius.append(temp_list[i][8])   
+    
+df2 = pd.DataFrame(list(zip(Star_names,Distance,Mass,Radius)),columns=['Star_name','Distance','Mass','Radius'])
+print(df2)
+
+df2.to_csv('dawrf_stars.csv')
